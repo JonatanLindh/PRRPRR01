4782fb0c51cd029819ff3682994de3762a0ee015
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:37:44 +0200

    Övning 19

---
+++ Del 1 - Grunderna/Övning 19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Övning_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Starttal: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Sluttal: ");
+            int slut = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            for (int i = start; i <= slut; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
