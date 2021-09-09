ba0ecf21b049e52040c2383b5c0f807266c644e7
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:54:24 +0200

    Övning 22

---
+++ Del 1 - Grunderna/Övning 22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Övning_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Saldo på konto (kr): ");
+            int balance = int.Parse(Console.ReadLine());
+            
+            Console.Write("Ränta (%): ");
+            double interest = 1 + int.Parse(Console.ReadLine()) / 100.0;
+            
+            Console.Write("Mål (kr): ");
+            int goal = int.Parse(Console.ReadLine());
+
+            int years = (int) Math.Ceiling(Math.Log(goal / balance, interest));
+            double finalBalance = balance * Math.Pow(interest, years);
+            
+            Console.WriteLine($"\nDet kommer att ta {years} år innan saldot når {finalBalance :f2}");
+
+
+        }
+    }
+}
