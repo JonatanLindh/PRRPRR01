70c747500335ed41549ddc5a895850d9d12d8257
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 21:01:26 +0200

    Övning 23

---
+++ Del 1 - Grunderna/Övning 23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Övning_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Start: ");
+            int start = int.Parse(Console.ReadLine());
+            
+            Console.Write("Stop: ");
+            int stop = int.Parse(Console.ReadLine());
+            
+            Console.Write("Steg: ");
+            int step = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            
+            for (int i = start; i <= stop; i+=step)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
