e29f53582dd431da52b8898a994d95a421032682
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:31:16 +0200

    Övning 16

---
+++ Del 1 - Grunderna/Övning 16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Övning_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Multipel: ");
+            int mult = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10 ; i++)
+            {
+                Console.WriteLine(mult * i);
+            }
+        }
+    }
+}
