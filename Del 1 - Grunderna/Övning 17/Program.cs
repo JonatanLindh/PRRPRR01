152e0676017b746cfb6631e297605c13fc1644c3
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:32:37 +0200

    Övning 17

---
+++ Del 1 - Grunderna/Övning 17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Övning_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sluttal: ");
+            int stop = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i <= stop ; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
