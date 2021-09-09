26bf000d5ac5810958ba2daa151dded501321ad7
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 21:10:35 +0200

    Övning 24

---
+++ Del 1 - Grunderna/Övning 24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Övning_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 0;
+            long m = 1;
+
+            while (n < 1_000_000)
+            {
+                Console.WriteLine(m);
+                long next = n + m;
+                n = m;
+                m = next;
+            };
+        }
+    }
+}
