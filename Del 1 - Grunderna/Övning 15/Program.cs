5189df87d8cd77ac1d9ffb430f9aa0043037e68d
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:28:46 +0200

    Övning 15

---
+++ Del 1 - Grunderna/Övning 15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Övning_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Del 1 - Grunderna/Övning14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Övning14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
