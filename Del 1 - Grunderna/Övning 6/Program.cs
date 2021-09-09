05ff682d0c8cf810b48b3defa5808955f2d0e982
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 18:32:03 +0200

    Övning 6

---
+++ Del 1 - Grunderna/Övning 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Övning_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Grader i celsius: ");
+            double degCelsius = double.Parse(Console.ReadLine());
+            double degFahrenheit = 1.8 * degCelsius + 32;
+            Console.WriteLine($"Grader i Fahrenheit: {degFahrenheit:f2}");
+        }
+    }
+}
