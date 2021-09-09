2401fac28d90e8106cc8201db8a75baaf32f100d
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:41:29 +0200

    Övning 20

---
+++ Del 1 - Grunderna/Övning 20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Övning_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            while (sum < 1_000_000)
+            {   
+                Console.Write("Tal: ");
+                sum += long.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"\nSumma: {sum}");
+        }
+    }
+}
