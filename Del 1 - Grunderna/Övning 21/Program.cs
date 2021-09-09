e2ede2e86f6c8bcd7cb45c06c7b8f46c65fed96e
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-09 20:44:05 +0200

    Övning 21

---
+++ Del 1 - Grunderna/Övning 21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Övning_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine();
+            
+            while (true)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
