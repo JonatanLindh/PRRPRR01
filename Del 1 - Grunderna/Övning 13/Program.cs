81763d53a9316955f69075a85f44baa04b47da58
Jonatan Lindh <0428lijo@jonkoping.se>
2021-09-13 13:37:26 +0200

    Övning 13

--- Del 1 - Grunderna/Övning 13/Program.cs
+++ Del 1 - Grunderna/Övning 13/Program.cs
@@ -3,6 +3,12 @@ using System;
 using System.Diagnostics;
 using Subclasses;
 
+/*
+ * Subclasses innehåller classes som jag inheritar från
+ * Detta är för att inte behöva upprepa kod
+ * https://github.com/JonatanLindh/PRRPRR01/blob/master/Del%201%20-%20Grunderna/Subclasses/Class1.cs
+ * Härifrån kommer t.ex Input metoden
+ */
 
 namespace Övning_13
 {
