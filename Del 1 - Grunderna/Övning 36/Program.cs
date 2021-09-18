e9548c5b3ec06e042acdca2bae08582a81862fd4
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-18 13:40:17 +0200

    Övning 36

---
+++ Del 1 - Grunderna/Övning 36/Program.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+using System;
+using Subclasses;
+
+/*
+ * Subclasses innehåller classes som jag inheritar från
+ * Detta är för att inte behöva upprepa kod
+ * https://github.com/JonatanLindh/PRRPRR01/blob/master/Del%201%20-%20Grunderna/Subclasses/Class1.cs
+ * Härifrån kommer t.ex Input metoden
+ */
+
+namespace Övning_36
+{
+    class Program : MainSubclass
+    {
+        static void Main(string[] args)
+        {
+            int num = Input("Tal: ", int.Parse);
+            Console.WriteLine(num switch
+            {
+                < 0 => "\nTalet är negativt",
+                0 => "\nTalet är 0",
+                > 0 => "\nTalet är positivt",
+            });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
