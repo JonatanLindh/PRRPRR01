4f18b4fc0c83b6e5bc4e2d30a95cb7548761e795
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-09-18 15:04:03 +0200

    CreateParser & Övning 37

--- Del 1 - Grunderna/Subclasses/Class1.cs
+++ Del 1 - Grunderna/Subclasses/Class1.cs
@@ -1,5 +1,7 @@
 ﻿#nullable enable
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Subclasses
 {
@@ -23,10 +25,31 @@ namespace Subclasses
                 }
                 catch (Exception e)
                 {
-                    string errMsg = $"Kunde inte konvertera \"{input}\" till {typeof(T).Name} \nFel: {e.Message}";
+                    string errMsg = $"Ogiltig input: \"{input}\"\nFel: {e.Message}";
                     Console.WriteLine($"{errMsg}\n");
                 }
             }
+        }
+
+        public static Func<string, T> CreateParser<T>(Func<string, T> baseParser,
+            T[]? notAllowed=null, T[]? onlyAllowed=null, string? errMsg=null) where T : notnull
+        {
+            return s =>
+            {
+                T parsed = baseParser(s);
+                if (onlyAllowed != null && !onlyAllowed.Contains(parsed))
+                {
+                    throw new Exception((errMsg != null) ? errMsg : $"Input måste vara någon av dessa: {onlyAllowed}");
+                }
+                
+                if (notAllowed != null && notAllowed.Contains(parsed))
+                {
+                    throw new Exception((errMsg != null) ? errMsg : $"Input får inte vara någon av dessa: {onlyAllowed}");
+                    
+                }
+
+                return parsed;
+            };
         }
     }
 }
---
+++ Del 1 - Grunderna/Övning 37/Program.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using Subclasses;
+
+/*
+ * Subclasses innehåller classes som jag inheritar från
+ * Detta är för att inte behöva upprepa kod
+ * https://github.com/JonatanLindh/PRRPRR01/blob/master/Del%201%20-%20Grunderna/Subclasses/Class1.cs
+ * Härifrån kommer t.ex Input metoden
+ */
+
+
+namespace Övning_37
+{
+    class Program : MainSubclass
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Input("Första talet: ", double.Parse);
+
+            // Validerar att räknesätt ör +-*/
+            Func<string, char> operatorParser = CreateParser(char.Parse, onlyAllowed: new char[] {'+', '-', '*', '/'},
+                errMsg: "Okänt räknesätt.");
+            char op = Input("Räknesätt (+-*/): ", operatorParser);
+
+            // Ser till att num2 inte är 0 om räknesätt är division
+            Func<string, double> num2Parser = (op == '/')
+                ? CreateParser(double.Parse, new double[] {0}, errMsg: "Tal 2 kan inte vara 0 p.g.a. division")
+                : double.Parse;
+            double num2 = Input("Andra talet: ", num2Parser);
+
+            // Kan endast vara +-*/ pga operatorParser so default behövs inte, stänger därför av varningen
+# pragma warning disable 8509
+            double ans = op switch
+            {
+                '+' => num1 + num2,
+                '-' => num1 - num2,
+                '*' => num1 * num2,
+                '/' => num1 / num2,
+            };
+# pragma warning restore
+
+            Console.WriteLine($"\n{num1} {op} {num2} = {ans}");
+        }
+    }
+}
