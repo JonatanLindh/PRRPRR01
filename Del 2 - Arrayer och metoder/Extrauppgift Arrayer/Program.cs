ff2e1c1b2e7c2fea8ad4738e57a367354c1758c9
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-10-25 13:57:46 +0200

    arrayer extra

---
+++ Del 2 - Arrayer och metoder/Extrauppgift Arrayer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Superclasses;
+
+namespace Extrauppgift_Arrayer
+{
+    class Program : MainSuperclass
+    {
+        static void Main(string[] args)
+        {
+            PirateLang();
+        }
+
+        static void PirateLang()
+        {
+            string indata = Input("Skriv något: ", s => s);
+            string result = "";
+            char[] vowels_and_space = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y','Å', 'Ä', 'Ö', ' ' };
+            
+            foreach (char letter in indata.ToUpper())
+            {
+                result += vowels_and_space.Contains(letter)
+                    ? letter.ToString().ToLower()
+                    : $"{letter}o{letter}".ToLower();
+            }
+            
+            Console.WriteLine($"\n{result}");
+        }
+        
+    }
+}
---
+++ Del 2 - Arrayer och metoder/Superclasses/Class1.cs
@@ -0,0 +1,54 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Superclasses
+{
+    public class MainSuperclass
+    {
+        public static T Input<T>(string prompt, Func<string, T> parser)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string? input = Console.ReadLine();
+                try
+                {
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        throw new ArgumentNullException();
+                    }
+
+                    var parsed = parser(input);
+                    return parsed;
+                }
+                catch (Exception e)
+                {
+                    string errMsg = $"Ogiltig input: \"{input}\"\nFel: {e.Message}";
+                    Console.WriteLine($"{errMsg}\n");
+                }
+            }
+        }
+        
+        public static Func<string, T> CreateParser<T>(Func<string, T> baseParser,
+            T[]? notAllowed=null, T[]? onlyAllowed=null, string? errMsg=null) where T : notnull
+        {
+            return s =>
+            {
+                T parsed = baseParser(s);
+                if (onlyAllowed != null && !onlyAllowed.Contains(parsed))
+                {
+                    throw new Exception(errMsg ?? $"Input måste vara någon av dessa: [{string.Join(", ", onlyAllowed!)}]");
+                }
+                
+                if (notAllowed != null && notAllowed.Contains(parsed))
+                {
+                    throw new Exception(errMsg ?? $"Input får inte vara någon av dessa: [{string.Join(", ", onlyAllowed!)}]");
+                }
+
+                return parsed;
+            };
+        }
+    }
+}
