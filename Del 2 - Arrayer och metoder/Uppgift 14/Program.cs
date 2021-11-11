a24bef6f1f0f54f1e13424e5b3632d7a06a1eb6e
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-11-11 08:50:06 +0100

    Uppgift 14

---
+++ Del 2 - Arrayer och metoder/Uppgift 14/Program.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+using System;
+using System.Text.RegularExpressions;
+using Superclasses; // https://github.com/JonatanLindh/PRRPRR01/blob/master/Del%202%20-%20Arrayer%20och%20metoder/Superclasses/Class1.cs
+
+namespace Uppgift_14
+{
+    class Program : MainSuperclass
+    {
+        static void Main(string[] args)
+        {
+            string email = Input("Mail: ", s => s);
+            Console.WriteLine();
+
+            Regex re = new Regex(@"^.+@.+\..+$");
+            Console.WriteLine(re.IsMatch(email) ? "Godkänd" : "Ej godkänd");
+        }
+    }
+}
