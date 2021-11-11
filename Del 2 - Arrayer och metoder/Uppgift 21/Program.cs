65fd17b12649f3145fa15780f0e353909adaa9ab
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-11-11 17:08:33 +0100

    Uppgift 21

---
+++ Del 2 - Arrayer och metoder/Uppgift 21/Program.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System;
+using System.Globalization;
+using
+    Superclasses; // https://github.com/JonatanLindh/PRRPRR01/blob/master/Del%202%20-%20Arrayer%20och%20metoder/Superclasses/Class1.cs
+
+
+namespace Uppgift_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int nextYear = 2022;
+            for (int i = nextYear; i < nextYear + 10; i++)
+            {
+                var bd = new DateTime(i, 3, 28);
+                Console.WriteLine($"{i}: {bd.DayOfWeek}");
+            }
+        }
+    }
+}
