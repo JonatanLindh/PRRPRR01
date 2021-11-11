335765a02b24d0ebcd09796f3b316375ff7baad4
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-11-11 09:04:05 +0100

    Uppgift 14

--- Del 2 - Arrayer och metoder/Extrauppgift Arrayer/Program.cs
+++ Del 2 - Arrayer och metoder/Extrauppgift Arrayer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using Superclasses;
+#nullable enable
 
 namespace Extrauppgift_Arrayer
 {
@@ -9,24 +10,22 @@ namespace Extrauppgift_Arrayer
         static void Main(string[] args)
         {
             // PirateLangEncrypt();
-            CaesarEncrypt(new [] {1, 2, 3});
+            // CaesarEncrypt(new [] {1, 2, 3});
             // CaesarDecrypt();
-            // Console.WriteLine(-1 % 2);
+            PirateLangDecrypt();
         }
 
         static void PirateLangEncrypt()
         {
             string indata = Input("Skriv något: ", s => s);
             string result = "";
-            char[] vowels_and_space = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y','Å', 'Ä', 'Ö', ' ' };
-            
-            foreach (char letter in indata.ToUpper())
+            char[] consonants =
+                {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'z', 'x'};
+            foreach (char letter in indata.ToLower())
             {
-                result += vowels_and_space.Contains(letter)
-                    ? letter.ToString().ToLower()
-                    : $"{letter}o{letter}".ToLower();
+                result += consonants.Contains(letter) ? $"{letter}o{letter}" : letter.ToString();
             }
-            
+
             Console.WriteLine($"\n{result}");
         }
 
@@ -35,13 +34,22 @@ namespace Extrauppgift_Arrayer
             string indata = Input("Skriv något: ", s => s.ToLower());
             string result = "";
 
-            const int offset = 97;
+            const int unicodeOffset = 97;
+            const int nLetters = 26;
+            int keyPos = 0;
             for (int i = 0; i < indata.Length; i++)
             {
-                result += Char.IsLetter(indata[i])
-                    ? (char) (offset + (((int) indata[i] - offset + key[i % key.Length]) % 26))
-                    : indata[i];
+                char l = indata[i];
+                if (!char.IsLetter(l))
+                {
+                    result += l;
+                    continue;
+                }
+
+                result += (char) (unicodeOffset + (l - unicodeOffset + key[keyPos % key.Length]) % nLetters);
+                keyPos++;
             }
+
             Console.WriteLine($"\n{result}");
         }
 
@@ -57,18 +65,82 @@ namespace Extrauppgift_Arrayer
 
                 return s.ToLower();
             });
-            
-            string result = "";
-            
-            const int offset = 97;
+
+            List<int> key = new List<int>();
+
+            const int nLetters = 26;
             for (int i = 0; i < enc.Length; i++)
             {
                 char e = enc[i];
                 char d = dec[i];
-                
+
+                if (!char.IsLetter(e)) continue;
+                key.Add((nLetters + (e - d)) % nLetters);
             }
-            
-            Console.WriteLine($"\n{result}");
+
+            for (int i = 1; i <= key.Count; i++)
+            {
+                List<int> looping = Enumerable
+                    .Repeat(
+                        key.GetRange(0, i),
+                        (int) Math.Ceiling(key.Count / (double) i) + 1
+                    )
+                    .SelectMany(a => a)
+                    .ToList();
+
+                if (!looping.GetRange(0, key.Count).SequenceEqual(key)) continue;
+
+                Console.WriteLine($"\nNyckel: {string.Join(' ', key.GetRange(0, i))}");
+                return;
+            }
+        }
+
+        static void PirateLangDecrypt()
+        {
+            var enc = Input("Skriv något krypterat: ", s => s.ToLower().GetEnumerator());
+            string dec = "";
+            char[] consonants =
+                {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'z', 'x'};
+            while (enc.MoveNext())
+            {
+                char current = enc.Current;
+                if (consonants.Contains(current))
+                {
+                    enc.MoveNext();
+                    enc.MoveNext();
+                }
+
+                dec += current;
+            }
+
+            Console.WriteLine($"\n{dec}");
+        }
+    }
+    
+    public class MainSuperclass
+    {
+        public static T Input<T>(string prompt, Func<string, T> parser)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string? input = Console.ReadLine();
+                try
+                {
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        throw new ArgumentNullException();
+                    }
+
+                    var parsed = parser(input);
+                    return parsed;
+                }
+                catch (Exception e)
+                {
+                    string errMsg = $"Ogiltig input: \"{input}\"\nFel: {e.Message}";
+                    Console.WriteLine($"{errMsg}\n");
+                }
+            }
         }
     }
 }
