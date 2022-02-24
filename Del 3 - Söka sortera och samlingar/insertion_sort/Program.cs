b3ca1c60e11900e65edafef74ad5aa768923cab6
Jonatan Lindh <jonatan.lindh1@gmail.com>
2022-02-24 08:55:33 +0100

    Insertion Sort

---
+++ Del 3 - Söka sortera och samlingar/insertion_sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace insertion_sort
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int[] array = CreateArray(20);
+            Console.WriteLine("Unsorted array:");
+            PrintArray(array);
+
+            InsertionSort(array);
+            Console.WriteLine("Sorted array:");
+            PrintArray(array);
+        }
+
+        static int[] CreateArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+            }
+            return array;
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void InsertionSort(int[] array)
+        {
+            int temp, idx;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                idx = i;
+                while (array[idx] < array[idx - 1])
+                {
+                    temp = array[idx];
+                    array[idx] = array[idx - 1];
+                    array[idx - 1] = temp;
+                    idx--;
+                    if (idx == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
