199c8082245075abfd8a9379a21db3abab43b242
Jonatan Lindh <jonatan.lindh1@gmail.com>
2022-03-23 14:57:20 +0100

    Uppg 14

---
+++ Del 3 - Söka sortera och samlingar/uppg_14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uppg_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(word) ? "Palindrome" : "Not palindrome");
+        }
+        static bool IsPalindrome(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                stack.Push(c);
+            }
+            foreach (char c in s)
+            {
+                if (stack.Pop() != c)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
